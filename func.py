3fe2ddc893a597ee05a8cf6c31708e92d626d3fd
Mark Fynes <tfynes@vmware.com>
2023-06-09 14:10:28 -0400

    .

--- func.py
+++ func.py
@@ -2,5 +2,5 @@ from typing import Any
 
 def test(req: Any):
     # Your function implementation goes here
-    print('Hello World!')
-    return "Hello World!"
+    print('Hello World')
+    return "Hello World"
